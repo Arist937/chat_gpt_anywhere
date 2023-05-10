5464dee754e0306e04cc90f7c1dc2bb90373205d
Arist937 <Aristotle937@gmail.com>
2023-05-10 16:33:57 -0400

    backend changes

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,10 @@ import openai
 
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -14,13 +18,29 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 
+openai.organization = os.environ["ORGANIZATION"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+@app.route("/process_page", methods=["POST"])
+def process_page():
+  body = request.json["body"]
+  soup = BeautifulSoup(body)
+  
+  print(soup.prettify())
+  
 
 @app.route("/complete_chat", methods=["POST"])
 def complete_chat():
-    user_input = request.json["user_input"]
+    user_input = request.json["user_input"]    
 
-    openai.organization = os.getenv("ORGANIZATION")
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Moderation.create(
+      input=user_input
+    )
+    
+    if response.results[0]['flagged']:
+      return {
+        "message": "Your message contains inappropiate content."
+      }
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{user_input}"}]
