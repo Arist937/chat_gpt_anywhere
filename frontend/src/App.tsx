2979cbf21b5d1b4c67014d337eabdb65297d4e84
Matthew Leung <matthewleung87.ml@gmail.com>
2023-05-11 18:56:09 -0400

    Get page html and POST

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  const [pageProcessed, setPageProcessed] = useState(false)
-  const [userInput, setUserInput] = useState('')
-  const [message, setMessage] = useState('')
+  const [pageProcessed, setPageProcessed] = useState<boolean | null>(null);
+  const [userInput, setUserInput] = useState('');
+  const [message, setMessage] = useState('');
+
+  const DOMtoString = (selector: any) => {
+    if (selector) {
+        selector = document.querySelector(selector);
+        if (!selector) return "ERROR: querySelector failed to find node"
+    } else {
+        selector = document.documentElement;
+    }
+    return selector.outerHTML;
+  };
+
+  const processPage = async () => {
+    var message = document.querySelector('#message');
+
+    chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+        var activeTab = tabs[0];
+        var activeTabId = activeTab.id;
+
+        return chrome.scripting.executeScript({
+            target: { tabId: activeTabId as number },
+            func: DOMtoString,
+            args: ['body']
+        });
+
+    }).then(function (results) {
+      console.log(results[0].result);
+      fetch("http://localhost:5000/process_page", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "body": results[0].result
+        }),
+      }).then(() => setPageProcessed(true));
+    }).catch(function (error) {
+      console.error('There was an error injecting script : \n' + error.message);
+      setPageProcessed(false);
+    });
+  };
 
   const testFunction = async () => {
     const response = await fetch("http://localhost:5000/complete_chat", {
@@ -24,6 +64,8 @@ function App() {
 
     setMessage(data.message)
   }
+
+  useEffect(() => {processPage()}, []);
 
   return (
     <div className="app">
